aeff7cc06269a46daf93582f607ef66f16be12d7
Sadduser <vadim.pankratov.04@gmail.com>
2023-02-22 18:39:03 +0500

    Added a menu burger

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ for (let anchor of anchors) {
   })
 }
 
+let burger_menu = () => {
+  let btn_open_menu = document.querySelector('.burger_button')
+  let burger_menu = document.querySelector('.burger-menu')
+  let btn_close_menu = document.querySelector('.burger_close_btn')
+  btn_open_menu.addEventListener('click', () => {
+    burger_menu.classList.toggle('opened')
+  })
+  btn_close_menu.addEventListener('click', () => {
+    burger_menu.classList.remove('opened')
+  })
+
+}
+
+
+burger_menu() 
